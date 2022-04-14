06c876efadb11e952d326061bbe5b62c6d4afaa8
franciscoguido-99 <francisconguido@gmail.com>
2022-04-13 22:01:29 -0300

    Error Navbar

---
+++ src/components/Catalogo.js
@@ -0,0 +1,17 @@
+/*const Catalogo = () => {
+  return (
+    <div class="flex space-x-4">
+      <div class="flex-auto w-32">
+        <img src="golf.jpg"></img>
+      </div>
+      <div class="flex-auto w-32">
+        <img src="golf.jpg"></img>
+      </div>
+      <div class="flex-auto w-32">
+        <img src="golf.jpg"></img>
+      </div>
+    </div>
+  );
+};
+
+export default Catalogo; */
---
+++ src/components/Saludo.js
@@ -0,0 +1,10 @@
+const Saludo = ({ name, edad }) => {
+  return (
+    <div>
+      <h1>Hola {name}</h1>
+      <h2>Tu edad es: {edad}</h2>
+    </div>
+  );
+};
+
+export default Saludo;
