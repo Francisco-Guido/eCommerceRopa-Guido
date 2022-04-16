b0d2785604657ead93b1eec2b369fb9c2dad45f7
franciscoguido-99 <francisconguido@gmail.com>
2022-04-16 15:19:15 -0300

    Creando ItemListContainer y CartWidget

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from "./logo.svg";
+import "./App.css";
 import NavBar from "./components/NavBar";
+import ItemListContainer from "./components/ItemListContainer";
 import Catalogo from "./components/Catalogo";
 import Saludo from "./components/Saludo";
-import "./App.css";
-
+import logo from "./logo.svg";
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <ItemListContainer name="acá esta el ItemListContainer :)" />
     </div>
   );
 }
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,9 @@
+const ItemListContainer = ({ name }) => {
+  return (
+    <div>
+      <h1>Hola, {name}</h1>
+    </div>
+  );
+};
+
+export default ItemListContainer;
